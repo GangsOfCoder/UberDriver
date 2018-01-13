78ee9a7b2e87c60a86dbfe7a2e8ad739be4c770a
Suraj Bahadur <suraj.bahadur2@gmail.com>
2018-01-14 00:11:27 +0530

    added path from location a to location b

---
+++ app/src/main/java/gangsofcoder/uberdriver/common/common.java
@@ -0,0 +1,8 @@
+package gangsofcoder.uberdriver.common;
+
+/**
+ * Created by suraj on 13-Jan-18.
+ */
+
+public class common {
+}
---
+++ app/src/main/java/gangsofcoder/uberdriver/remote/RetrofitClient.java
@@ -0,0 +1,8 @@
+package gangsofcoder.uberdriver.remote;
+
+/**
+ * Created by suraj on 13-Jan-18.
+ */
+
+public class RetrofitClient {
+}
